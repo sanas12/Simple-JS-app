42e94dd82383881dcd6ae61560e7602bb9d87363
sanas12 <162470494+sanas12@users.noreply.github.com>
2024-03-12 18:19:07 +0100

    Added code to highlight special pokemon

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-let pokemonList=[];
-
-pokemonList.prop({
-    name: "Charmander",
-    height: 6,
-    types: ['fire']
-  });
+let pokemonList = [
+    { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
+    { name: "Charmander", height: 6, types: ['fire'] },
+    { name: "Squirtle", height: 5, types: ['water'] }
+  ];
   
-  pokemonList.prop({
-    name: "Squirtle",
-    height: 5,
-    types: ['water']
-  });
+  // Set the height threshold for special Pokémon
+  const specialPokemonThreshold = 6;
   
-  console.log(pokemonList); 
-
-
-
+  // Loop through the pokemonList array
+  for (let i = 0; i < pokemonList.length; i++) {
+    // Write Pokémon name and height to the DOM using document.write()
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+  
+    // Check if the height is above the special threshold
+    if (pokemonList[i].height > specialPokemonThreshold) {
+      // Add the label for special Pokémon
+      document.write(" - Wow, that’s big!");
+    }
+  
+  document.write("<br>");
+    
+  }
