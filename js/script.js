8a3e845d6cf82c24d196961b450f6aa1aaaaf01e
sanas12 <162470494+sanas12@users.noreply.github.com>
2024-03-12 16:32:23 +0100

    Implemented variable with key values

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-alert('Hello World');
-let favoriteFood = 'Italian';
-document.write(favoriteFood);
+let pokemonList=[];
+
+pokemonList.objects({
+    name: "Charmander",
+    height: 6,
+    types: ['fire']
+  });
+  
+  pokemonList.objects({
+    name: "Squirtle",
+    height: 5,
+    types: ['water']
+  });
+  
+  console.log(pokemonList); 
+
+
+
