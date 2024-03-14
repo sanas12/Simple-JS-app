e0d01b4a1b71849768d16b50a84d3edcf5be5c4a
sanas12 <162470494+sanas12@users.noreply.github.com>
2024-03-14 22:40:46 +0100

    Add IIFE and pokemon

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
 let pokemonList = [
-    { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
-    { name: "Charmander", height: 6, types: ['fire'] },
-    { name: "Squirtle", height: 5, types: ['water'] }
-  ];
-  
-  // Set the height threshold for special Pokémon
-  const specialPokemonThreshold = 6;
-  
-  // Loop through the pokemonList array
-  //for (let i = 0; i < pokemonList.length; i++) {
-    // Write Pokémon name and height to the DOM using document.write()
-   // document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
-   pokemonList.forEach(function(pokemon)
-   {
-    document.write(pokemon.name + " (height: " + pokemon.height + ")");
-   
-  
-    // Check if the height is above the special threshold
-    if (pokemon.height > specialPokemonThreshold) {
-      // Add the label for special Pokémon
-      document.write(" - Wow, that’s big!");
-    }
-    document.write("<br>");
+  { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
+  { name: "Charmander", height: 6, types: ['fire'] },
+  { name: "Squirtle", height: 5, types: ['water'] }
+];
 
-  } );
-   
-    
+// Set the height threshold for special Pokémon
+const specialPokemonThreshold = 6;
+
+let pokemonRepository = (function () {
+ 
+  function getAll() {
+    return pokemonList;
+  }
   
+  function add(pokemon) {
+    
+    pokemonList.push(pokemon);
+  }
+  // Return public functions
+  return {
+    getAll: getAll,
+    add: add
+  };
+  
+  
+})();
+
+pokemonRepository.add({ name: 'Pikachu', height: 4, types: ['electric'] });
+
+// Iterate over each Pokémon in the repository
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
+
+  // Check if the height is above the special threshold
+  if (pokemon.height > specialPokemonThreshold) {
+    // Add the label for special Pokémon
+    document.write(" - Wow, that’s big!");
+  }
+  
+  // Add line break for better formatting
+  document.write("<br>");
+});
