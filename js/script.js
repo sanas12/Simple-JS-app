c6b8937d0bdf8b2123b33476280664f37940acaf
sanas12 <162470494+sanas12@users.noreply.github.com>
2024-03-14 17:00:49 +0100

    Changed for loop to forEach

--- js/script.js
+++ js/script.js
@@ -8,16 +8,22 @@ let pokemonList = [
   const specialPokemonThreshold = 6;
   
   // Loop through the pokemonList array
-  for (let i = 0; i < pokemonList.length; i++) {
+  //for (let i = 0; i < pokemonList.length; i++) {
     // Write Pokémon name and height to the DOM using document.write()
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+   // document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+   pokemonList.forEach(function(pokemon)
+   {
+    document.write(pokemon.name + " (height: " + pokemon.height + ")");
+   
   
     // Check if the height is above the special threshold
-    if (pokemonList[i].height > specialPokemonThreshold) {
+    if (pokemon.height > specialPokemonThreshold) {
       // Add the label for special Pokémon
       document.write(" - Wow, that’s big!");
     }
-  
-  document.write("<br>");
+    document.write("<br>");
+
+  } );
+   
     
-  }
+  
